08680fa765933d92c50f2e9fab0f0e647e4558dd
Sniped <facto@facto.club>
2020-11-05 08:33:53 -0500

    remove svg text color

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
 	return (
 		<div>
 			<svg
-				className="h-16 w-16 ml-24 fill-current text-green-600"
+				className="h-16 w-16 ml-24 fill-current"
 				xmlns="http://www.w3.org/2000/svg"
 				fill="none"
 				viewBox="0 0 24 24"
